470901e0c74ea3e01150dcc660669f52ad3170f4
João Victor Novais <joao.dipaay@gmail.com>
2023-07-18 12:15:58 -0300

    Swagger Documentation

--- src/main/java/com/joao/gamevault/GameVaultApplication.java
+++ src/main/java/com/joao/gamevault/GameVaultApplication.java
@@ -3,7 +3,11 @@ package com.joao.gamevault;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Game-Vault API", version = "1.0", description = "A Vault of Top Games API"))
 public class GameVaultApplication {
 
 	public static void main(String[] args) {
